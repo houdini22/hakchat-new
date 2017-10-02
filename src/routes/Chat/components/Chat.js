0866917448677766bb93625d6dfc9591ad6156d8
hud <baniczek@gmail.com>
2017-10-02 04:05:51 +0200

    chat css

--- src/routes/Chat/components/Chat.js
+++ src/routes/Chat/components/Chat.js
@@ -29,11 +29,11 @@ export class ChatView extends React.Component {
   render () {
     return (
       <div styleName='chat-container'>
-        <Row>
-          <Col md={3} className={`${styles['col']} ${styles['col-1']}`}>
+        <Row className={`${styles['row']}`}>
+          <Col md={3} sm={4} xs={12} className={`${styles['col']} ${styles['col-1']}`}>
 
           </Col>
-          <Col md={9} className={`${styles['col']} ${styles['col-2']}`}>
+          <Col md={9} sm={8} xs={12} className={`${styles['col']} ${styles['col-2']}`}>
             <div styleName='message-container'>
               <input
                 type='text'
